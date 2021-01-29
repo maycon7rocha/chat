22e03c214a585232e52a27d1d5392cbe6f1e2d94
Maycon Rocha <maycon7rocha@gmail.com>
2021-01-29 17:23:57 -0300

    aula 351

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -10,6 +11,20 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(10),
           child: Text('Funcionou'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chat')
+              .snapshots()
+              .listen((querySnapshot) {
+             //print(querySnapshot.documents[0]['text']);
+              querySnapshot.documents.forEach((element) {
+                print(element['text']);
+              });
+          });
+        },
       ),
     );
   }

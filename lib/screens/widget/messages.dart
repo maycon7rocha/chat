e42a87ecaeff8aa25bdd7bbec694ed1c7a89304a
Maycon Rocha <maycon7rocha@gmail.com>
2021-02-09 12:28:18 -0300

    364

--- lib/screens/widget/messages.dart
+++ lib/screens/widget/messages.dart
@@ -1,3 +1,4 @@
+import 'package:chat/screens/widget/message_bobble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -20,7 +21,7 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs.length,
-          itemBuilder: (ctx, i) => Text(
+          itemBuilder: (ctx, i) => MessageBubble(
             chatDocs[i]['text'],
           ),
         );
